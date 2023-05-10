95bd05af152c0c6985baadf8a36904ce575276a3
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-05-10 07:54:03 +0100

    Remove items from database
    Imported remove function and added an event listener that removes an item from the database when that item is clicked

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-b8e6e-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -50,6 +50,11 @@ function appendItemToShoppingListEl(item) {
     let newEl = document.createElement("li")
 
     newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function () {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        remove(exactLocationOfItemInDB)
+    })
 
     shoppingListEl.append(newEl)
 }
