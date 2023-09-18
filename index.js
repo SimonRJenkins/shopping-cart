82c52155b2bbbc0dce03e5ff4a85106ede914269
SimonRJenkins <105888107+SimonRJenkins@users.noreply.github.com>
2023-09-18 02:53:35 +0100

    Update index.js
    Add an if else statement to prevent empty items from being submitted, added an alert box for the else

--- index.js
+++ index.js
@@ -1,69 +1,74 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-    databaseURL: "https://realtime-database-b8e6e-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  databaseURL:
+    "https://realtime-database-b8e6e-default-rtdb.europe-west1.firebasedatabase.app/",
+};
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const shoppingListEl = document.getElementById("shopping-list")
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function () {
-    let inputValue = inputFieldEl.value
+  let inputValue = inputFieldEl.value;
 
-    push(shoppingListInDB, inputValue)
-
-    clearInputFieldEl()
-})
+  if (inputValue.trim() !== "") {
+    push(shoppingListInDB, inputValue);
+    clearInputFieldEl();
+  } else {
+    alert("You must enter an item.")
+  }
+});
 
 onValue(shoppingListInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
 
-    if (snapshot.exists()) {
-        let itemsArray = Object.entries(snapshot.val())
+    clearShoppingListEl();
 
-        clearShoppingListEl()
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemId = currentItem[0];
+      let currentItemValue = currentItem[1];
 
-        for (let i = 0; i < itemsArray.length; i++) {
-            let currentItem = itemsArray[i]
-            let currentItemId = currentItem[0]
-            let currentItemValue = currentItem[1]
-
-            appendItemToShoppingListEl(currentItem)
-        }
+      appendItemToShoppingListEl(currentItem);
     }
-    else {
-        shoppingListEl.textContent = "No items here... yet"
-    }
-
-})
-
+  } else {
+    shoppingListEl.textContent = "No items here... yet";
+  }
+});
 
 function clearShoppingListEl() {
-    shoppingListEl.innerHTML = ""
+  shoppingListEl.innerHTML = "";
 }
 
 function clearInputFieldEl() {
-    inputFieldEl.value = ""
+  inputFieldEl.value = "";
 }
 
 function appendItemToShoppingListEl(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
+  let itemID = item[0];
+  let itemValue = item[1];
 
-    let newEl = document.createElement("li")
+  let newEl = document.createElement("li");
 
-    newEl.textContent = itemValue
+  newEl.textContent = itemValue;
 
-    newEl.addEventListener("click", function () {
-        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        remove(exactLocationOfItemInDB)
-    })
+  newEl.addEventListener("click", function () {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+    remove(exactLocationOfItemInDB);
+  });
 
-    shoppingListEl.append(newEl)
+  shoppingListEl.append(newEl);
 }
-
